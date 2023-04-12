e8ef2d0721cd98ec8c5cac9948cd87213f927107
IT-Cube <nikitaprokofev115@gmail.com>
2023-04-12 17:57:42 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def first_inf():
+    name = input('Привет, как тебя зовут? ')
+    age = int(input('Сколько тебе лет? '))
+    print(f'Привет {name}, тебе походу {age} лет.')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    first_inf()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
